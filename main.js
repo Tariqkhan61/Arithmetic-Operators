ce8f9eb3f82706c1b5d85c2f8e242e9fcd987f14
Tariqkhan61 <161859910+Tariqkhan61@users.noreply.github.com>
2024-03-14 05:46:04 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+//AIRTHMETIC OPERATORS
+/*let a=10;
+let b=5;
+let c=(a+b);
+let d=(a-b);
+let e=(a*b);
+let f=(a/b);
+console.log(c);  // 15
+console.log(d);  // 5
+console.log(e);  //50
+console.log(f);  // 2
+
+console.log(a==b); //false
+console.log(a > b); //true
+console.log(a < b); //false
+console.log(a % b); // 0
+
+let n1=5;
+let n=n1+15
+console.log(n);// Ans=20*/
+var num1 = 10;
+var num2 = 2;
+var add = num1 + num2;
+var sub = num1 - num2;
+var div = num1 / num2;
+var multi = num1 * num2;
+console.log(add); // 12
+console.log(sub); // 8
+console.log(div); // 5
+console.log(multi); // 20
+var x = 10;
+var y = 5;
+var z = 10;
+var isEqualxy = x == y;
+var isEqualxz = x == z;
+var isEqual = y == z;
+console.log(isEqualxy); //false
+console.log(isEqualxz); // true
+console.log(isEqual); // false
+// Exponent (**)
+var dates = 4;
+var apples = 5;
+var total = Math.pow(4, 5);
+console.log(total);
+// Modulus
+var samosas = 9;
+var roll = 4;
+var remainder = samosas % roll;
+console.log(remainder);
---
+++ main.ts
@@ -0,0 +1,56 @@
+//AIRTHMETIC OPERATORS (+ , - , * , /)
+
+let a=10;
+let b=5;
+let c=(a+b);  
+let d=(a-b);  
+let e=(a*b);  
+let f=(a/b);  
+console.log(c);  // 15
+console.log(d);  // 5
+console.log(e);  //50
+console.log(f);  // 2
+// = = , greater than, less than and modulus
+console.log(a==b); //false
+console.log(a > b); //true
+console.log(a < b); //false
+console.log(a % b); // 0
+
+let n1=5;
+let n=n1+15
+console.log(n);// Ans=20
+const num1:number=10;
+const num2:number=2
+const add=num1+num2; 
+const sub=num1-num2; 
+const div=num1/num2; 
+const multi=num1*num2;
+console.log(add);// 12
+console.log(sub);// 8
+console.log(div);// 5
+console.log(multi);// 20
+// = =
+let x: number = 10;
+let y: number = 5;
+let z:number=10;
+
+let isEqualxy:boolean = x == y; 
+let isEqualxz: boolean = x == z;  
+let isEqual: boolean = y == z;  
+  
+console.log(isEqualxy); //false
+console.log(isEqualxz); // true
+console.log(isEqual); // false
+
+// Exponent (**)
+let dates:number=4;
+let apples:number=5;
+let total:number=4**5
+console.log(total);
+
+// Modulus
+let samosas:number=9;
+let roll:number=4;
+let remainder:number=samosas % roll;
+console.log(remainder);
+
